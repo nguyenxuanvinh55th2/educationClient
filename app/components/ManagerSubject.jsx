9549f0a2b2b48da729cbaa1c497e3c24857c9e0a
Vinh Nguyen <nguyenxuanvinh55th2@gmail.com>
2017-04-27 21:18:59 +0700

    tmp

--- app/components/ManagerSubject.jsx
+++ app/components/ManagerSubject.jsx
@@ -22,10 +22,15 @@ class ManagerSubject extends React.Component {
       },
       dataTheme: {
         isTheme: true,
-        files: []
+        title: '',
+        content: '',
+        files: [],
+        open: false
       },
       dataAssign: {
         isAssignment: true,
+        title: '',
+        content: '',
         files: []
       }
     }
@@ -35,12 +40,37 @@ class ManagerSubject extends React.Component {
       data: {
         isForum: true,
         content: this.state.dataForum.content,
-        subjectId: this.state.subjectId
       },
+      classSubjectId: this.state.subjectId,
       files: this.state.dataForum.files
     }
-    console.log(info);
-    this.props.insertForum(localStorage.getItem('Meteor.loginToken'),JSON.stringify(info)).then(({data}) => {
+    this.props.insertTopic(localStorage.getItem('Meteor.loginToken'),JSON.stringify(info)).then(({data}) => {
+      console.log(data);
+    })
+  }
+  handleAddTheme(){
+    let info = {
+      data: {
+        isTheme: true,
+        title: this.state.dataTheme.title,
+        content: this.state.dataTheme.content,
+      },
+      classSubjectId: this.state.subjectId
+    }
+    this.props.insertTopic(localStorage.getItem('Meteor.loginToken'),JSON.stringify(info)).then(({data}) => {
+      console.log(data);
+    })
+  }
+  handleAddAss(){
+    let info = {
+      data: {
+        isAssignment: true,
+        title: this.state.dataAssign.title,
+        content: this.state.dataAssign.content,
+      },
+      classSubjectId: this.state.subjectId,
+    }
+    this.props.insertTopic(localStorage.getItem('Meteor.loginToken'),JSON.stringify(info)).then(({data}) => {
       console.log(data);
     })
   }
@@ -221,24 +251,39 @@ class ManagerSubject extends React.Component {
                 <p>Chu de 2</p>
               </div>
               <button type="button" className="btn" style={{backgroundColor: 'white', border: '1px dotted #35bcbf', color: '#35bcbf', marginTop: 5, height: 40}}
-                 onClick={() => this.handleAddTheme()}>
-                <span className="glyphicon glyphicon-plus"></span> Thêm chủ đề
+                 onClick={() => {
+                   let dataTheme = this.state.dataTheme;
+                   dataTheme.open = !this.state.dataTheme.open;
+                   this.setState({dataTheme: dataTheme});
+                 }}>
+                <span className="glyphicon glyphicon-plus"></span> {this.state.dataTheme.open ? 'Hủy chủ đề' : 'Thêm chủ đề'}
               </button>
             </div>
-            <div style={{display: 'flex', flexDirection: 'column', marginTop: 10}}>
-              <div style={{backgroundColor: 'white', padding: 20}}>
-                <input type="text" placeholder="Tên chủ đề" style={{width: '100%', height: 40, padding:10, border: '1px solid #f0f0f0'}}/>
-                <div style={{border: '1px solid #f0f0f0', height: 100, padding: 10, marginTop: 15}}>
-                  <textarea rows="2" placeholder="Thêm nội dung chủ đề" style={{border: 'none', height: 55, width: '100%'}}/>
-                  <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}}>
-                    <button type="button" className="btn btn-link" style={{color: '#35bcbf'}}>Mở rộng</button>
+            {
+              this.state.dataTheme.open &&
+              <div style={{display: 'flex', flexDirection: 'column', marginTop: 10}}>
+                <div style={{backgroundColor: 'white', padding: 20}}>
+                  <input type="text" placeholder="Tên chủ đề" style={{width: '100%', height: 40, padding:10, border: '1px solid #f0f0f0'}} onChange={({target}) => {
+                    let dataTheme = this.state.dataTheme;
+                    dataTheme.title = target.value;
+                    this.setState({dataTheme: dataTheme});
+                  }}/>
+                  <div style={{border: '1px solid #f0f0f0', height: 100, padding: 10, marginTop: 15}}>
+                    <textarea rows="2" placeholder="Thêm nội dung chủ đề" style={{border: 'none', height: 55, width: '100%'}} onChange={({target}) => {
+                      let dataTheme = this.state.dataTheme;
+                      dataTheme.content = target.value;
+                      this.setState({dataTheme: dataTheme});
+                    }}/>
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}}>
+                      <button type="button" className="btn btn-link" style={{color: '#35bcbf'}}>Mở rộng</button>
+                    </div>
+                  </div>
+                  <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', marginTop: 10}}>
+                    <button type="button" className="btn" style={{backgroundColor: '#35bcbf', color: 'white', width: 100}} disabled={!this.state.dataTheme.title || !this.state.dataTheme.content} onClick={() => this.handleAddTheme()}>Thêm chủ đề</button>
                   </div>
                 </div>
-                <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', marginTop: 10}}>
-                  <button type="button" className="btn" style={{backgroundColor: '#35bcbf', color: 'white', width: 100}}>Thêm chủ đề</button>
-                </div>
               </div>
-            </div>
+            }
           </TabPanel>
           <TabPanel style={{backgroundColor: '#f0f0f0'}}>
             <div style={{display: 'flex', flexDirection: 'column', padding: 20, backgroundColor: 'white'}}>
@@ -256,15 +301,23 @@ class ManagerSubject extends React.Component {
             <div style={{display: 'flex', flexDirection: 'column', marginTop: 10}}>
               <div style={{backgroundColor: 'white', padding: 20}}>
                 <button type="button" className="btn btn-link" style={{color: '#35bcbf'}}>Tạo mới bài tập tự luận</button>
-                <input type="text" placeholder="Tên bài tập" style={{width: '100%', height: 40, padding:10, border: '1px solid #f0f0f0'}}/>
+                <input type="text" placeholder="Tên bài tập" style={{width: '100%', height: 40, padding:10, border: '1px solid #f0f0f0'}} onChange={({target}) => {
+                  let dataAssign = this.state.dataAssign;
+                  dataAssign.title = target.value;
+                  this.setState({dataAssign: dataAssign});
+                }}/>
                 <div style={{border: '1px solid #f0f0f0', height: 100, padding: 10, marginTop: 15}}>
-                  <textarea rows="2" placeholder="Thêm nội dung câu hỏi bài tập" style={{border: 'none', height: 55, width: '100%'}}/>
+                  <textarea rows="2" placeholder="Thêm nội dung câu hỏi bài tập" style={{border: 'none', height: 55, width: '100%'}} onChange={({target}) => {
+                    let dataAssign = this.state.dataAssign;
+                    dataAssign.content = target.value;
+                    this.setState({dataAssign: dataAssign});
+                  }}/>
                   <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end'}}>
                     <button type="button" className="btn btn-link" style={{color: '#35bcbf'}}>Mở rộng</button>
                   </div>
                 </div>
                 <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', marginTop: 10}}>
-                  <button type="button" className="btn" style={{backgroundColor: '#35bcbf', color: 'white', width: 100}}>Thêm bài tập</button>
+                  <button type="button" className="btn" style={{backgroundColor: '#35bcbf', color: 'white', width: 100}} disabled={!this.state.dataAssign.title || !this.state.dataAssign.content} onClick={() => this.handleAddAss()}>Thêm bài tập</button>
                 </div>
               </div>
             </div>
@@ -285,8 +338,8 @@ class ManagerSubject extends React.Component {
   }
 }
 const INSERT_FORUM = gql`
- mutation insertForum($token:String!,$info:String){
-   insertForum(token:$token,info:$info)
+ mutation insertTopic($token:String!,$info:String){
+   insertTopic(token:$token,info:$info)
  }
 `;
 const MyQuery = gql`
@@ -315,7 +368,7 @@ export default compose(
   }),
   graphql(INSERT_FORUM,{
        props:({mutate})=>({
-       insertForum : (token,info) =>mutate({variables:{token,info}})
+       insertTopic : (token,info) =>mutate({variables:{token,info}})
      })
    })
 )(ManagerSubject)
