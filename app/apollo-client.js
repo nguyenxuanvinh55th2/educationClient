8c2e4c129747715d7bd63926ee3a33656d8e1adf
Vinh Nguyen <nguyenxuanvinh55th2@gmail.com>
2017-05-13 23:11:24 +0700

    tmp

--- app/apollo-client.js
+++ app/apollo-client.js
@@ -1,19 +1,33 @@
 import ApolloClient,{createNetworkInterface} from 'apollo-client';
-import { Client } from 'subscriptions-transport-ws';
+// import { Client } from 'subscriptions-transport-ws';
+//
+// // import login from './login'
+// import addGraphQLSubscriptions from './addGraphQL.js'
+//
+// //conect websocket on server
+// const wsClient = new Client('ws://localhost:3000');
+// //conect graphql
+// const networkInterface = createNetworkInterface({ uri: 'http://localhost:3000/graphql' })
+//
+// const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
+//   networkInterface,
+//   wsClient,
+// );
+//
+// export const client = new ApolloClient({
+//   networkInterface: networkInterfaceWithSubscriptions,
+// })
 
-// import login from './login'
-import addGraphQLSubscriptions from './addGraphQL.js'
-
-//conect websocket on server
-const wsClient = new Client('ws://localhost:8090');
-//conect graphql
-const networkInterface = createNetworkInterface({ uri: 'http://localhost:8080/graphql' })
-
-const networkInterfaceWithSubscriptions = addGraphQLSubscriptions(
-  networkInterface,
-  wsClient,
-);
-
-export const client = new ApolloClient({
-  networkInterface: networkInterfaceWithSubscriptions,
-})
+import { graphqlUrl } from './config';
+let ApolloClientCase;
+const networkInterface = createNetworkInterface({ uri: graphqlUrl });
+if(caseProto === 'local'){
+    //dev
+    ApolloClientCase = new ApolloClient({
+      networkInterface
+    });
+} else {
+    //Replace with this when build
+     ApolloClientCase = new ApolloClient();
+}
+export const client = ApolloClientCase;
