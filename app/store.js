03b54b7d8266ca3d940f843e67881c036a1b7320
vinh.nguyen <vinh.nguyen@s2corp.vn>
2017-06-12 21:24:51 +0700

    tmp

--- app/store.js
+++ app/store.js
@@ -1,7 +1,7 @@
 import rootReducer from './reducers';
 import { client } from './apollo-client.js';
 import { createStore, compose, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
 import {routerReducer, syncHistoryWithStore} from 'react-router-redux'
 import { browserHistory} from 'react-router'
 import { socketEndpoint } from './config';
